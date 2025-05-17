c17ed86ca3ab4343e16c0b060f946707a472756f
John Korhel <john@korhel.net>
2025-05-17 15:33:18 -0600

    remove client type, not needed

--- frontend/src/lib/honoClient.ts
+++ frontend/src/lib/honoClient.ts
@@ -2,5 +2,3 @@ import { hc } from "hono/client";
 import type { AppType } from "@fossai/backend";
 
 export const client = hc<AppType>("http://localhost:3000");
-
-export type Client = ReturnType<typeof hc<AppType>>;
