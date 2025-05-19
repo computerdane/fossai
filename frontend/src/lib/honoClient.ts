2ba75cf9f27d9b8dbc264c79a8df5b16d896362a
Dane Rieber <danerieber@gmail.com>
2025-05-19 18:58:55 -0500

    VITE_BACKEND_BASE_URL env variable for frontend

--- frontend/src/lib/honoClient.ts
+++ frontend/src/lib/honoClient.ts
@@ -1,8 +1,11 @@
 import { hc } from "hono/client";
 import type { AppType } from "@fossai/backend";
 
-export const client = hc<AppType>("http://localhost:3000", {
-  init: {
-    credentials: "include",
+export const client = hc<AppType>(
+  import.meta.env.VITE_BACKEND_BASE_URL ?? "http://localhost:3000",
+  {
+    init: {
+      credentials: "include",
+    },
   },
-});
+);
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+interface ViteTypeOptions {
+  strictImportMetaEnv: true;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_BACKEND_BASE_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
